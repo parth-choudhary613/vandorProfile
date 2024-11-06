6d78a69d58ab16eaf73f8564e4b693be10db212d
Parth Choudhary <parthchoudhary4372@gmail.com>
2024-11-06 08:54:54 +0530

    routingchanges

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Profile from '../src/profileSection/profileSection'
+import Profile from '../src/profileSection/profileSection';
 
 const App = () => {
   return (
-    <Router>
-     <Routes>
-     <Route exact path="/" element={<Profile/>} />
-      
-        </Routes>
+    <Router basename="/vendorprofile">
+      <Routes>
+        <Route path="/" element={<Profile />} />
+      </Routes>
     </Router>
   );
 };
